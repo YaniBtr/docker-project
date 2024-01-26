134307200f8de646cfa51497d17203db5a821cb1
Yani BETROUNE <yaniakbou@gmail.com>
2024-01-26 22:21:15 +0100

    edited js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         yani
+         yani betr
         </p>
         <a
           className="App-link"
